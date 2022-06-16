8826f3ea6e4ec1af1bc83ada9da04216c2051e96
Stalicites <88872425+stalicites@users.noreply.github.com>
2022-06-16 16:40:47 -0700

    Update ui.js

--- ui.js
+++ ui.js
@@ -8,11 +8,21 @@ const idInput = document.getElementById("id-container");
 const errorHeader = document.getElementById("header");
 
 const endPoint = `https://discord-lookup-server.herokuapp.com`;
+const colorThief = new ColorThief();
 
 function uToD(unix) {  
     let stamp = moment.unix(unix/1000)
     return stamp.format('MM/DD/YY - h:mm:ss A');
 }   
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+  
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
 
 function fetchDataByUser(id) {
     let q = new XMLHttpRequest();
@@ -37,13 +47,45 @@ function fetchDataByUser(id) {
         document.getElementById("profile-image").src = res.avatar + "?size=1024";
         document.getElementById("created-at").innerText = `Created at: ${uToD(res.createdAt)}`
         document.getElementById("username").innerText = `Username: ${res.username}`;
-        document.getElementById("banner-color").innerText = `Banner Color: ${res.bannerColor}`;
+        if (res.bannerColor != null) {
+            document.getElementById("banner-color").innerText = `Banner Color: ${res.bannerColor}`;
+        }
+        if (res.badge.length != 0) {
+            let seen = {}
+            res.badge.forEach((badge) => {
+                if (badge != "verifiedBot") {    
+                    let img = document.createElement("img");
+                    img.src = `svgs/${badge}.svg`;
+                    img.className = "badge";
+                    document.getElementById("badges").appendChild(img);
+                } else {
+                    if (seen["verifiedBot"]) {
+                        let p = document.createElement("p");
+                        p.innerText = "BOT";
+                        p.className = "bot"
+                        document.getElementById("badges").appendChild(p);
+                    }
+                }
+                seen[badge] = true;
+            })
+        } else {
+            document.getElementById("holder-badges").innerText = `Badges: None!`;
+        }
         //document.getElementById("banner-color").style.backgroundColor = res.bannerColor;
         document.getElementById("query-id").innerText = `Query ID: ${res.query}`;
-        document.getElementById("badges").innerText = `Badges: ${res.badge}`;
         document.getElementById("profile-image-link").href = res.avatar + "?size=1024"
         idPaste.className = "container hidden"
         panel.className = "container visible";
+
+        document.getElementById("profile-image").onload = function() {
+            if (res.bannerColor == null) {
+                let img = document.getElementById("profile-image")
+                img.setAttribute('crossOrigin', '');
+                let colorAvg = colorThief.getColor(img);
+                colorAvg = rgbToHex(colorAvg[0], colorAvg[1], colorAvg[0])
+                document.getElementById("banner-color").innerText = `Banner Color: ${colorAvg}`
+            }
+        }
     }
     q.onerror = function(e) {
         console.log(e);
@@ -54,7 +96,6 @@ function fetchDataByUser(id) {
             if (index != 1) {
                 errorHeader.className = ""
                 index++;
-                console.log("RUNNING");
             } else {
                 clearInterval();
             }
@@ -77,4 +118,10 @@ idInput.onkeyup = function(e) {
 goBack.onclick = function() {
     idPaste.className = "container visible"
     panel.className = "container hidden";
+    document.getElementById("badges").innerHTML = 
+    `
+    <div id = "badges">
+        <p id = "holder-badges">Badges: </p>
+    </div>
+    `
 }
